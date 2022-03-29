a81af28b8f66f94d6bbbaa996cd27325b49530fd
FitriNurindahsari <fitrinurindahsari26@gmail.com>
2022-03-29 13:54:29 +0700

    Add New File - tentang.php (About Us Page)

---
+++ tentang.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title -->
+    <title>Hotel Aston</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="assets/hotel-pages/img/core-img/hotel.png">
+
+    <!-- Core Stylesheet -->
+    <link rel="stylesheet" href="assets/hotel-pages/style.css">
+    <link rel="stylesheet" href="assets/plugins/fontawesome-free/css/all.min.css">
+
+
+</head>
+
+<body>
+    <!-- Preloader -->
+    <div class="preloader d-flex align-items-center justify-content-center">
+        <div class="preloader-circle"></div>
+        <div class="preloader-img">
+            <img src="assets/hotel-pages/img/core-img/hotel.png" alt="">
+        </div>
+    </div>
+
+    <!-- ##### Header Area Start ##### -->
+    <header class="header-area">
+
+        <!-- ***** Top Header Area ***** -->
+        <div class="top-header-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="top-header-content d-flex align-items-center justify-content-between">
+                            <!-- Top Header Content -->
+                            <div class="top-header-meta">
+                                <a href="#" data-toggle="tooltip" data-placement="bottom" title="+1 234 122 122"><i class="fa fa-phone" aria-hidden="true"></i> <span>Call Us: +1 234 122 122</span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- ***** Navbar Area ***** -->
+        <div class="alazea-main-menu">
+            <div class="classy-nav-container breakpoint-off">
+                <div class="container">
+                    <!-- Menu -->
+                    <nav class="classy-navbar justify-content-between" id="alazeaNav">
+
+                        <!-- Nav Brand -->
+                        <a href="index.html" class="nav-brand"><img src="img/core-img/logo.png" alt=""></a>
+
+                        <!-- Navbar Toggler -->
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span></span>
+                        </div>
+
+                        <!-- Menu -->
+                        <div class="classy-menu">
+
+                            <!-- Close Button -->
+                            <div class="classycloseIcon">
+                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                            </div>
+
+                            <!-- Navbar Start -->
+                            <div class="classynav">
+                                <ul>
+                                    <li><a href="index.php">Produk</a></li>
+                                    <li><a href="harga.php">Daftar harga</a></li>
+                                    <li><a href="pesan.php">Pesan kamar</a></li>
+                                    <li><a href="tentang.php">Tentang Kami</a></li>
+                                </ul>
+                                <!-- Search Icon -->
+
+                            </div>
+                            <!-- Navbar End -->
+                        </div>
+                    </nav>
+
+                    <!-- Search Form -->
+                    <div class="search-form">
+                        <form action="#" method="get">
+                            <input type="search" name="search" id="search" placeholder="Type keywords &amp; press enter...">
+                            <button type="submit" class="d-none"></button>
+                        </form>
+                        <!-- Close Icon -->
+                        <div class="closeIcon"><i class="fa fa-times" aria-hidden="true"></i></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ##### Header Area End ##### -->
+
+    <!-- ##### Breadcrumb Area Start ##### -->
+    <div class="breadcrumb-area">
+        <!-- Top Breadcrumb Area -->
+        <div class="top-breadcrumb-area bg-img bg-overlay d-flex align-items-center justify-content-center" style="background-image: url(assets/hotel-pages/img/bg-img/hotel.jpg);">
+            <h2>Tentang Kami</h2>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.php"><i class="fa fa-home"></i>Produk</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Tentang Kami</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ##### Breadcrumb Area End ##### -->
+
+    <!-- ##### Contact Area Info Start ##### -->
+    <div class="contact-area-info section-padding-0-100">
+    <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-12 col-lg-5">
+                    <!-- Section Heading -->
+                    <div class="section-heading">
+                        <h2>ASTON Priority Simatupang and Conference Center</h2>
+                        <p>
+                        Dengan menginap di Horison Suites & Residences Rasuna Jakarta, Anda akan berada di pusat kota Jakarta, hanya 10 menit dengan berkendara dari Bundaran Hi dan Monumen Nasional. Aparthotel yang cocok untuk keluarga ini berada 1,9 mi (3,1 km) dari Taman Suropati dan 3,5 mi (5,6 km) dari Grand Indonesia.
+                        Nikmati kenyamanan rumah Anda sendiri di salah satu dari 213 kamar yang dilengkapi dengan perabotan berbeda-beda, yang dilengkapi dengan dapur dengan lemari es/freezer ukuran biasa dan oven microwave. Kamar memiliki balkon pribadi. Televisi layar datar 29-inci dengan program satelit menyediakan hiburan, sementara akses Internet nirkabel gratis siap menjamin koneksi Anda. Kenyamanan meliputi brankas dan meja tulis.
+                        Alamat kami di 505 Silk Rd, Jakarta. Hubungi Kami di nomor +1234122122 dan alamat email info.deercreative@gmail.com.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="col-12 col-lg-6">
+                    <div class="alazea-benefits-area">
+                        <div class="row">
+                            <!-- Single Benefits Area -->
+                            <div class="col-12 col-sm-6">
+                                <div class="single-benefits-area">
+                                    <img src="assets/hotel-pages/img/core-img/b1.png" alt="">
+                                    <h5>Quality Products</h5>
+                                    <p>Website ini menampilkan informasi terkait details harga kamar hotel yang berkualitas dan lengkap.</p>
+                                </div>
+                            </div>
+
+                            <!-- Single Benefits Area -->
+                            <div class="col-12 col-sm-6">
+                                <div class="single-benefits-area">
+                                    <img src="assets/hotel-pages/img/core-img/b2.png" alt="">
+                                    <h5>Perfect Service</h5>
+                                    <p>Kami melayani dengan senang hati sesuai dengan keluhan dan apa yang anda butuhkan.</p>
+                                </div>
+                            </div>
+
+                            <!-- Single Benefits Area -->
+                            <div class="col-12 col-sm-6">
+                                <div class="single-benefits-area">
+                                    <img src="assets/hotel-pages/img/core-img/b3.png" alt="">
+                                    <h5>100% Natural</h5>
+                                    <p>Kamar hotel dengan pemandangan alam yang indah.</p>
+                                </div>
+                            </div>
+
+                            <!-- Single Benefits Area -->
+                            <div class="col-12 col-sm-6">
+                                <div class="single-benefits-area">
+                                    <img src="assets/hotel-pages/img/core-img/b4.png" alt="">
+                                    <h5>Environmentally friendly</h5>
+                                    <p>Hotel kami sangat peduli dengan lingkungan sehingga pelayanan kami selalu berusaha yang tidak merusak lingkungan dan tidak memberi dampak buruk atau negative terhadap lingkungan sekitar.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ##### Contact Area Info End ##### -->
+    
+    <!-- ##### Footer Area Start ##### -->
+    <footer class="footer-area bg-img" style="background-image: url(img/bg-img/3.jpg);">
+        <!-- Main Footer Area -->
+        <div class="main-footer-area">
+            <div class="container">
+                <div class="row">
+
+                    <!-- Single Footer Widget -->
+                    <div class="col-12 col-sm-4 col-lg-4">
+                        <div class="single-footer-widget">
+                            <div class="widget-title">
+                                <h5>info Website</h5>
+                            </div>
+                            <nav class="widget-nav">
+                                <p>Merupakan website untuk memesan kamar hotel di hotel Aston.</p>
+                            </nav>
+                        </div>
+                    </div>
+
+                    <!-- Single Footer Widget -->
+                    <div class="col-12 col-sm-4 col-lg-4">
+                        <div class="single-footer-widget">
+                        <div class="widget-title">
+                                <h5>LINK</h5>
+                            </div>
+                            <nav class="widget-nav">
+                                <ul>
+                                    <li><a href="index.php">Produk</a></li>
+                                    <li><a href="harga.php">Daftar harga</a></li>
+                                    <li><a href="pesan.php">Pesan kamar</a></li>
+                                    <li><a href="tentang.php">Tentang Kami</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+
+                    <!-- Single Footer Widget -->
+                    <div class="col-12 col-sm-4 col-lg-4">
+                        <div class="single-footer-widget">
+                            <div class="widget-title">
+                                <h5>CONTACT</h5>
+                            </div>
+
+                            <div class="contact-information">
+                                <p><span>Address:</span> 505 Silk Rd, Jakarta</p>
+                                <p><span>Phone:</span> +1 234 122 122</p>
+                                <p><span>Email:</span> info.deercreative@gmail.com</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Footer Bottom Area -->
+        <div class="footer-bottom-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="border-line"></div>
+                    </div>
+                    <!-- Copywrite Text -->
+                    <div class="col-12 col-md-6">
+                        <div class="copywrite-text">
+                            <p>Copyright &copy; 2020</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- ##### Footer Area End ##### -->
+
+    <!-- ##### All Javascript Files ##### -->
+    <!-- jQuery-2.2.4 js -->
+    <script src="assets/hotel-pages/js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="assets/hotel-pages/js/bootstrap/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="assets/hotel-pages/js/bootstrap/bootstrap.min.js"></script>
+    <!-- All Plugins js -->
+    <script src="assets/hotel-pages/js/plugins/plugins.js"></script>
+    <!-- Active js -->
+    <script src="assets/hotel-pages/js/active.js"></script>
+</body>
+
+</html>
