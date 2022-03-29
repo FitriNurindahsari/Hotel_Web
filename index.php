000b431472177ecc19516fe4c3f5ef7987fdb07a
FitriNurindahsari <fitrinurindahsari26@gmail.com>
2022-03-29 11:52:25 +0700

    Add New File - index.php (Homepagel)

---
+++ index.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title -->
+    <title>Hotel Aston</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="assets/hotel-pages/img/core-img/hotel.png">
+
+    <!-- Core Stylesheet -->
+    <link rel="stylesheet" href="assets/hotel-pages/style.css">
+    <link rel="stylesheet" href="assets/plugins/fontawesome-free/css/all.min.css">
+
+</head>
+
+<body>
+    <!-- Preloader -->
+    <div class="preloader d-flex align-items-center justify-content-center">
+        <div class="preloader-circle"></div>
+        <div class="preloader-img">
+            <img src="assets/hotel-pages/img/core-img/hotel.png" alt="">
+        </div>
+    </div>
+
+    <!-- ##### Header Area Start ##### -->
+    <header class="header-area">
+
+        <!-- ***** Top Header Area ***** -->
+        <div class="top-header-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="top-header-content d-flex align-items-center justify-content-between">
+                            <!-- Top Header Content -->
+                            <div class="top-header-meta">
+                                <a href="#" data-toggle="tooltip" data-placement="bottom" title="+1 234 122 122"><i class="fa fa-phone" aria-hidden="true"></i> <span>Call Us: +1 234 122 122</span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- ***** Navbar Area ***** -->
+        <div class="alazea-main-menu">
+            <div class="classy-nav-container breakpoint-off">
+                <div class="container">
+                    <!-- Menu -->
+                    <nav class="classy-navbar justify-content-between" id="alazeaNav">
+
+                        <!-- Nav Brand -->
+                        <a href="index.html" class="nav-brand"><img src="img/core-img/logo.png" alt=""></a>
+
+                        <!-- Navbar Toggler -->
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span></span>
+                        </div>
+
+                        <!-- Menu -->
+                        <div class="classy-menu">
+
+                            <!-- Close Button -->
+                            <div class="classycloseIcon">
+                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                            </div>
+
+                            <!-- Navbar Start -->
+                            <div class="classynav">
+                                <ul>
+                                    <li><a href="index.php">Produk</a></li>
+                                    <li><a href="harga.php">Daftar harga</a></li>
+                                    <li><a href="pesan.php">Pesan kamar</a></li>
+                                    <li><a href="tentang.php">Tentang Kami</a></li>
+                                </ul>
+
+                            </div>
+                            <!-- Navbar End -->
+                        </div>
+                    </nav>
+
+                    <!-- Search Form -->
+                    <div class="search-form">
+                        <form action="#" method="get">
+                            <input type="search" name="search" id="search" placeholder="Type keywords &amp; press enter...">
+                            <button type="submit" class="d-none"></button>
+                        </form>
+                        <!-- Close Icon -->
+                        <div class="closeIcon"><i class="fa fa-times" aria-hidden="true"></i></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ##### Header Area End ##### -->
+
+    <!-- ##### Hero Area Start ##### -->
+    <section class="hero-area">
+        <div class="hero-post-slides owl-carousel">
+
+            <!-- Single Hero Post -->
+            <div class="single-hero-post bg-overlay">
+                <!-- Post Image -->
+                <div class="slide-img bg-img" style="background-image: url(assets/hotel-pages/img/bg-img/hotel.jpg);"></div>
+                <div class="container h-100">
+                    <div class="row h-50 align-items-center">
+                        <div class="col-12">
+                            <!-- Post Content -->
+                            <div class="hero-slides-content text-center">
+                                <h2 style="padding-top:150px">SELAMAT DATANG DI JAKARTA</h2>
+                                <p>ASTON Priority Simatupang and Conference Center yang strategis terletak di sebelah Arkadia Business Park dengan akses mudah ke Jakarta - Jalan Tol Bintaro ring road, menawarkan lingkungan yang dinamis dan peristirahatan mewah untuk setiap wisatawan.</p>
+                                <div class="welcome-btn-group">
+                                    <a href="pesan.php" class="btn alazea-btn mr-30">BOOKING</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Single Hero Post -->
+            <div class="single-hero-post bg-overlay">
+                <!-- Post Image -->
+                <div class="slide-img bg-img" style="background-image: url(assets/hotel-pages/img/bg-img/hotel1.jpg);"></div>
+                <div class="container h-100">
+                    <div class="row h-50 align-items-center">
+                        <div class="col-12">
+                            <!-- Post Content -->
+                            <div class="hero-slides-content text-center">
+                                <h2 style="padding-top:150px">ASTON Priority Simatupang and Conference Center</h2>
+                                <p>Hotel ini memiliki lokasi ideal untuk mengeksplorasi pemandangan dan atraksi tak terbatas dari salah satu kota yang paling menarik di Asia. Sebuah tempat menenangkan dan tenang di jantung kota Jakarta Selatan. Baik itu mengunjungi untuk bisnis ataupun liburan, nikmati kenyamanan rumah di kamar dan suite  kami yang dirancang dengan cantik.</p>
+                                <div class="welcome-btn-group">
+                                    <a href="pesan.php" class="btn alazea-btn mr-30">BOOKING</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ##### Hero Area End ##### -->
+
+    <section class="alazea-portfolio-area section-padding-100-0">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <!-- Section Heading -->
+                    <div class="section-heading text-center">
+                        <h2>Daftar Tipe Kamar</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid">
+                <div class="modal fade" id="video" aria-labelledby="myModalLabel">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <video controls>
+                                    <source src="assets/hotel-pages/video/hotel.mp4" type="video/mp4" />
+                                </video>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <p><img src="assets/hotel-pages/img/core-img/Family Suite.jfif" id='imgleft'><strong>Family Suite</strong><br>
+                    1 Queen Bed and 1 Twin Bed. 861-sq-foot individually furnished room, balcony with city views. Layout - Bedroom, dining area, and sitting area. Relax - In-room massage available. Internet - Free WiFi. Entertainment - 29-inch flat-screen TV with premium channels. Food & Drink - Kitchen with refrigerator, microwave, cookware/dishware, and coffee/tea maker
+                    Sleep - Blackout drapes/curtains and bed sheets. Bathroom - Private bathroom, shower, bathrobes, and slippers.<br>
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#video">Video</button> <a href="harga.php"><button type="button" class="btn btn-primary">Harga</button></a></p>
+                </div>
+                <div class="row">
+                    <p><img src="assets/hotel-pages/img/core-img/Business Suite.jfif" id='imgright'><strong>Business Suite</strong><br>
+                    1 Queen Bed. 689-sq-foot individually furnished room, balcony with city views. Layout - Bedroom, dining area, and sitting area. Relax - In-room massage available. Internet - Free WiFi. Entertainment - 29-inch flat-screen TV with premium channels. Food & Drink - Kitchen with refrigerator, microwave, cookware/dishware, and coffee/tea maker.
+                    Sleep - Blackout drapes/curtains and bed sheets. Bathroom - Private bathroom, shower, bathrobes, and slippers.<br>
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#video">Video</button> <a href="harga.php"><button type="button" class="btn btn-primary">Harga</button></a></p>	
+                </div>
+                <div class="row">
+                    <p><img src="assets/hotel-pages/img/core-img/Grand Executive.jfif" id='imgleft'><strong>Grand Executive</strong><br>
+                    1 Queen Bed and 1 Twin Bed. 840-sq-foot individually furnished room, balcony with city views. Layout - Dining area and sitting area. Relax - In-room massage available. Internet - Free WiFi. Entertainment - 29-inch flat-screen TV with premium channels. Food & Drink - Kitchen with refrigerator, microwave, cookware/dishware, and coffee/tea maker
+                    Sleep - Blackout drapes/curtains and bed sheets. Bathroom - Private bathroom, shower, bathrobes, and slippers.<br>
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#video">Video</button> <a href="harga.php"><button type="button" class="btn btn-primary">Harga</button></a></p>	
+                </div>
+                <div class="row">
+                    <p><img src="assets/hotel-pages/img/core-img/Deluxe Double Room With Bath.jfif" id='imgright'><strong>Deluxe Double Room With Bath</strong><br>
+                    Property confirms they are implementing enhanced health & safety measures. 24 hour. front desk. Commonly-touched surfaces are cleaned with disinfectant. Fitness Center. Property is cleaned with disinfectant. Property confirms they are implementing guest safety measures. Gift Shop.
+                    Bed sheets and towels are washed at a temperature of at least 60°C/140°F. Hot Tub or Spa. Concierge. Property uses a professional cleaning service. ATM Machine. Tennis Court. Parking - Self. Airport Transportation. Internet Access - Free in room.<br>
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#video">Video</button><a href="harga.php"> <button type="button" class="btn btn-primary">Harga</button></a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- ##### Footer Area Start ##### -->
+    <footer class="footer-area bg-img" style="background-image: url(img/bg-img/3.jpg);">
+        <!-- Main Footer Area -->
+        <div class="main-footer-area">
+            <div class="container">
+                <div class="row">
+
+                    <!-- Single Footer Widget -->
+                    <div class="col-12 col-sm-4 col-lg-4">
+                        <div class="single-footer-widget">
+                            <div class="widget-title">
+                                <h5>info Website</h5>
+                            </div>
+                            <nav class="widget-nav">
+                                <p>Merupakan website untuk memesan kamar hotel di hotel Aston.</p>
+                            </nav>
+                        </div>
+                    </div>
+
+                    <!-- Single Footer Widget -->
+                    <div class="col-12 col-sm-4 col-lg-4">
+                        <div class="single-footer-widget">
+                        <div class="widget-title">
+                                <h5>LINK</h5>
+                            </div>
+                            <nav class="widget-nav">
+                                <ul>
+                                    <li><a href="index.php">Produk</a></li>
+                                    <li><a href="harga.php">Daftar harga</a></li>
+                                    <li><a href="pesan.php">Pesan kamar</a></li>
+                                    <li><a href="tentang.php">Tentang Kami</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+
+                    <!-- Single Footer Widget -->
+                    <div class="col-12 col-sm-4 col-lg-4">
+                        <div class="single-footer-widget">
+                            <div class="widget-title">
+                                <h5>CONTACT</h5>
+                            </div>
+
+                            <div class="contact-information">
+                                <p><span>Address:</span> 505 Silk Rd, Jakarta</p>
+                                <p><span>Phone:</span> +1 234 122 122</p>
+                                <p><span>Email:</span> info.deercreative@gmail.com</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Footer Bottom Area -->
+        <div class="footer-bottom-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="border-line"></div>
+                    </div>
+                    <!-- Copywrite Text -->
+                    <div class="col-12 col-md-6">
+                        <div class="copywrite-text">
+                            <p>Copyright &copy; 2020</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- ##### Footer Area End ##### -->
+
+    <!-- ##### All Javascript Files ##### -->
+    <!-- jQuery-2.2.4 js -->
+    <script src="assets/hotel-pages/js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="assets/hotel-pages/js/bootstrap/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="assets/hotel-pages/js/bootstrap/bootstrap.min.js"></script>
+    <!-- All Plugins js -->
+    <script src="assets/hotel-pages/js/plugins/plugins.js"></script>
+    <!-- Active js -->
+    <script src="assets/hotel-pages/js/active.js"></script>
+</body>
+
+</html>
